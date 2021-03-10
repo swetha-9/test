ec832ccc29f8e2901020977a40871aea5dd59bed
swetha-9 <swethakayal27@gmail.com>
2021-03-10 21:32:13 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ParentComponentComponent } from './parent-component/parent-component.component';
+import { ChildComponentComponent } from './child-component/child-component.component';
+import {TestInterceptor} from './test.interceptor';
+import{HTTP_INTERCEPTORS,HttpClientModule} from '@angular/common/http';
+import { OktaCheckComponentComponent } from './okta-check-component/okta-check-component.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponentComponent,
+    ChildComponentComponent,
+    OktaCheckComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass:TestInterceptor,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
